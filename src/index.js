a079db432cddfd0ed66aec2e4d064b3d883fadcc
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2025-05-23 21:58:40 +0530

    Finish the project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import { createServer } from 'node:http';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
-const server = createServer((_req, res) => {
+const pathnameToFilename = (urlPathName) => {
+  const filenameObj = { status: 200, filename: './index.html' };
+  if (urlPathName === '/') return filenameObj;
+
+  const isFileInStore = ['about', 'contact-me'].some(
+    (name) => `/${name}` === urlPathName,
+  );
+  if (isFileInStore)
+    return { ...filenameObj, filename: `./${urlPathName}.html` };
+
+  return { status: 404, filename: './404.html' };
+};
+
+const port = 8_080;
+const hostname = 'localhost';
+const serverHome = `http://${hostname}:${port}`;
+
+const server = createServer(async (req, res) => {
+  const { pathname } = new URL(`${serverHome}${req.url}`);
+  const { status, filename } = pathnameToFilename(pathname);
   res.setHeader('content-type', 'text/html');
-  res.statusCode = 200;
-  res.end('Hello World!');
+
+  try {
+    const fileContent = await fs.readFile(
+      path.resolve(import.meta.dirname, filename),
+    );
+    res.statusCode = status;
+    res.end(fileContent);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-const port = 8_000;
-const hostname = 'localhost';
 server.listen(port, hostname, () => {
-  console.log(`Server started at http://${hostname}:${port}`);
+  console.log(`Server started at ${serverHome}`);
 });
